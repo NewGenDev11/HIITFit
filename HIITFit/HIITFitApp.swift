d191966935306db305de9be9a46b0648bfa8bb8b
NewGenDev11 <fisher.curtis85@gmail.com>
2025-01-24 15:23:33 -0500

    Initial Commit

---
+++ HIITFit/HIITFitApp.swift
@@ -0,0 +1,17 @@
+//
+//  HIITFitApp.swift
+//  HIITFit
+//
+//  Created by Curtis Fisher on 1/24/25.
+//
+
+import SwiftUI
+
+@main
+struct HIITFitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
