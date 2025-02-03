63c95fb5285a8e95267a9a420f68f8d3c66f2b7b
NewGenDev11 <fisher.curtis85@gmail.com>
2025-02-02 21:05:16 -0500

    changes to project for sections 6-9 couldnt complete section 10

---
+++ HIITFit/Views/GradientBackground.swift
@@ -0,0 +1,38 @@
+//
+//  GradientBackground.swift
+//  HIITFit
+//
+//  Created by Curtis Fisher on 2/2/25.
+//
+
+
+
+import SwiftUI
+
+struct GradientBackground: View {
+    var gradient: Gradient {
+        let color1 = Color.purple;
+        let color2 = Color.blue;        let background = Color("background")
+        return Gradient(
+            stops: [
+                Gradient.Stop(color: color1, location: 0),
+                Gradient.Stop(color: color2, location: 0.9),
+                Gradient.Stop(color: background, location: 0.9),
+                Gradient.Stop(color: background, location: 1)
+            ])
+    }
+
+    var body: some View {
+        LinearGradient(
+            gradient: gradient,
+            startPoint: .top,
+            endPoint: .bottom)
+        .ignoresSafeArea()
+    }
+}
+
+struct GradientBackground_Previews: PreviewProvider {
+    static var previews: some View {
+        GradientBackground()
+    }
+}
